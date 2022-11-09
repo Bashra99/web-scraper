ac6a0eeb830a8b97a168545820adf1d7a2cfda8c
Bashra99 <telfahb@gmail.com>
2022-11-09 12:00:44 +0000

    feat: create a scraper file with 3 functions

--- scraper.py
+++ scraper.py
@@ -7,15 +7,30 @@ url = 'https://en.wikipedia.org/wiki/Petra'
 soup = BeautifulSoup(requests.get(url).content, 'html.parser')
 
 
+
 def get_citations_needed_count(url):
     """get the number of citations needed
     """    
-    return len(soup.find_all('sup', class_='noprint Inline-Template Template-Fact'))
-
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return len(soup.find_all("a",title="Wikipedia:Citation needed"))
+   
+obj_citations={}
 def get_citations_needed_report(url):
     """get the report of the citations needed
     """    
-    return [citation.text for citation in soup.find_all('p') if 'citation needed' in citation.text]
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    citation_needed_arr=soup.find_all(title="Wikipedia:Citation needed")
+    counter=0
+    for citation in citation_needed_arr:
+        counter+=1
+        report=citation.parent.parent.parent.text.strip()
+        obj_citations[f"citation needed {counter}" ]=report
+        print(f"citation needed {counter} : {report} \n ")
+    # return (obj_citations)
+
+
 
 def saved_json_data(citations):
     """save the data in json file
@@ -23,16 +38,16 @@ def saved_json_data(citations):
     with open('citations.json', 'w') as json_file:
         json.dump(citations, json_file)
 
-def main():
-    print(f'The number of citations needed is {get_citations_needed_count(url)}')
-    print(f'The report of the citations needed is {get_citations_needed_report(url)}')
-    saved_json_data(get_citations_needed_report(url))
-
-print(main())
+        
 
 
 
 if __name__ == "__main__":
-    print(get_citations_needed_count(url))
-    print(get_citations_needed_report(url))
-    saved_json_data(get_citations_needed_report(url))
+    print(f'The number of citations needed is {get_citations_needed_count(url)}')
+    # get_citations_needed_report(url)
+
+    saved_json_data(obj_citations)
+    get_citations_needed_report(url)
+
+
+
